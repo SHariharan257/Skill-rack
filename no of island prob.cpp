350d0ca456422fd2ebb763117356e96cfe5d2693
SHariharan257 <s.honey9441578116@gmail.com>
2026-06-10 19:58:45 +0530

    Add files via upload

---
+++ no of island prob.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int row, col;
+    void dfs(vector<vector<char> >& g, int i, int j) {
+        if (i < 0 || j < 0 || i >= row || j >= col || g[i][j] == '0')
+            return;
+        g[i][j] = '0';
+        dfs(g, i + 1, j);
+        dfs(g, i - 1, j);
+        dfs(g, i, j + 1);
+        dfs(g, i, j - 1);
+    }
+    int solve(vector<vector<char> >& g) {
+        row = g.size();
+        col = g[0].size();
+        int cnt = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+
+                if (g[i][j] == '1') {
+                    cnt++;
+                    dfs(g, i, j);
+                }
+            }
+        }
+        return cnt;
+    }
+};
+int main() {
+    int row, col;
+    cin >> row >> col;
+    vector<vector<char> > g(row, vector<char>(col));
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cin >> g[i][j];
+        }
+    }
+    Solution s;
+    cout << s.solve(g);
+    return 0;
+}
