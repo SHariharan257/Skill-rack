503edec01fdd2bb7b5ec6e4840d0210d01f925d8
S.Hariharan <s.honey9441578116@gmail.com>
2026-09-02 21:04:18 +0530

    Update last day where you can still stand.cpp

--- last day where you can still stand.cpp
+++ last day where you can still stand.cpp
@@ -1,55 +1,44 @@
-class DSU {
-    vector<int> root;
-    vector<int> size;
-public:
-    DSU(int n) : root(n), size(n, 1) { iota(root.begin(), root.end(), 0); }
-    int find(int x) {
-        if (root[x] != x)
-            root[x] = find(root[x]);
-        return root[x];
+class Solution{
+    vector<int> parent,size;
+    int find(int x){
+        if(parent[x]==x) return x;
+        return parent[x]=find(parent[x]);
     }
-    void unite(int x, int y) {
-        int rx = find(x);
-        int ry = find(y);
-        if (rx == ry)
-            return;
-        if (size[rx] > size[ry]) {
-            int t = rx;
-            rx = ry;
-            ry = t;
-        }
-        root[rx] = ry;
-        size[ry] += size[rx];
+    void unite(int a,int b){
+        a=find(a);
+        b=find(b);
+        if(a==b) return;
+        if(size[a]<size[b]) swap(a,b);
+        parent[b]=a;
+        size[a]+=size[b];
     }
-};
-class Solution {
 public:
-    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
-        DSU dsu(row * col + 2);
-        vector<vector<int>> grid(row, vector<int>(col, 0));
-        int dirs[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        for (int i = (int)cells.size() - 1; i >= 0; i--) {
-            int r = cells[i][0] - 1;
-            int c = cells[i][1] - 1;
-            grid[r][c] = 1;
-            int id1 = r * col + c + 1;
-            for (int k = 0; k < 4; k++) {
-                int nr = r + dirs[k][0];
-                int nc = c + dirs[k][1];
-
-                if (nr >= 0 && nr < row && nc >= 0 && nc < col &&
-                    grid[nr][nc] == 1) {
-                    int id2 = nr * col + nc + 1;
-                    dsu.unite(id1, id2);
+    int latestDayToCross(int row,int col,vector<vector<int>>&cells){
+        int total=row*col;
+        int top=total;
+        int bottom=total+1;
+        parent.resize(total+2);
+        size.assign(total+2,1);
+        iota(parent.begin(),parent.end(),0);
+        vector<vector<int>> grid(row,vector<int>(col,0));
+        int dr[4]={-1,1,0,0};
+        int dc[4]={0,0,-1,1};
+        for(int day=total-1;day>=0;day--){
+            int r=cells[day][0]-1;
+            int c=cells[day][1]-1;
+            grid[r][c]=1;
+            int node=r*col+c;
+            if(r==0) unite(node,top);
+            if(r==row-1) unite(node,bottom);
+            for(int i=0;i<4;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr>=0&&nr<row&&nc>=0&&nc<col&&grid[nr][nc]){
+                    unite(node,nr*col+nc);
                 }
             }
-            if (r == 0)
-                dsu.unite(0, id1);
-            if (r == row - 1)
-                dsu.unite(row * col + 1, id1);
-            if (dsu.find(0) == dsu.find(row * col + 1))
-                return i;
+            if(find(top)==find(bottom)) return day;
         }
-        return -1;
+        return 0;
     }
 };
