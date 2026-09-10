64b0f831034625bdf13c0e82b015730f63895e1a
S.Hariharan <s.honey9441578116@gmail.com>
2026-09-10 14:35:12 +0530

    Create signs of prefix sum.cpp

---
+++ signs of prefix sum.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void solve(){
+    int n; string s; cin>>n>>s;
+    if(s[0]=='0' || s.contains("00")){
+        cout<<"-1"<<endl; return;
+    }
+    int ans=-1;
+    if(s.contains("+-")||s.contains("-+")){
+        if(s.contains("+--+") || s.contains("-++-")){
+            ans=3;
+        }else{
+            ans=2;
+        }
+    }else{
+        ans=1;
+        for(int i=0;i<n;i+=2){
+            if(s[i]=='0'){
+                ans=2; 
+                break;
+            }
+        }
+    }
+    cout<<ans<<endl;
+}
+int main(){
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+}
